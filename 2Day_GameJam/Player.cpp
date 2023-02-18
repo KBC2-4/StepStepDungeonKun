64325cfc095ec5c5ea3f087cdb3e4d5008c77fd1
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2023-02-18 13:24:56 +0900

    プレイヤーに必要そうな、関数と変数を作成 GameMain作成されるまで待機。

--- 2Day_GameJam/Player.cpp
+++ 2Day_GameJam/Player.cpp
@@ -1,1 +1,39 @@
 #include "Player.h"
+
+Player::Player()
+{
+	player_x = 0;
+	player_y = 0;
+	player_mistake = false;
+}
+
+//-----------------------
+//描画処理以外
+//-----------------------
+void Player::Update() 
+{
+	
+}
+
+//-----------------------
+//描画処理以外
+//-----------------------
+void Player::Draw()const
+{
+
+}
+
+//-----------------------
+//HPチェック処理
+//-----------------------
+bool Player::CheckHp()
+{
+	if (hp < 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
