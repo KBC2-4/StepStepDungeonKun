77114b86c3dde77c13a9d26db69c6154f41c122b
tamayosekento <107020913+tamayosekento@users.noreply.github.com>
2023-02-18 13:21:25 +0900

    タイル画像用意

--- 2Day_GameJam/Stage.cpp
+++ 2Day_GameJam/Stage.cpp
@@ -1,1 +1,27 @@
 #include "Stage.h"
+#include"DxLib.h"
+
+Stage::Stage()
+{
+	LoadDivGraph("Resource/Images/Stage/color_tile.png", 4, 2, 2, 1, 1, tile_image);
+	**stage = 0;
+	next_tile = 0;
+	next_color = 0;
+}
+
+Stage::~Stage()
+{
+
+}
+
+void Stage::Update()
+{
+	next_tile = GetRand(1);
+	next_color = GetRand(3);
+	//DrawModiGraph(40,500,120,500,100,540,20,540,)
+}
+
+void Stage::Draw()
+{
+
+}
--- 2Day_GameJam/Stage.h
+++ 2Day_GameJam/Stage.h
@@ -1,5 +1,21 @@
 #pragma once
 class Stage
 {
-};
+private:
+	int tile_image[4];
 
+	short stage[2][5];
+
+	//次のタイル[0:空, 1:奥, 2:手前, 3:両方]
+	short next_tile;
+
+	//次のタイルの色[0:赤, 1:緑, 2:青, 3:黄]
+	short next_color;
+
+public:
+	Stage();
+	~Stage();
+	void Update();
+	void Draw();
+
+};
